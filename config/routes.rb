69543b279e8a8f43ee7aacb1e8db411ef1a84136
Luigi Guerreros <luigi.guerreros@gmail.com>
2016-07-22 06:19:42 +0000

    subiendo proyecto

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  devise_for :users
+  
+  devise_scope :user do
+  authenticated :user do
+    root 'welcome#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
